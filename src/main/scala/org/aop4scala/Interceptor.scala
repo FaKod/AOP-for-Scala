093dc4161222bbbbb8a198f4e7c617bcb98030b9
FaKod <info@fakod.eu>
2010-07-09 09:28:40 +0200

    make tests compilable

--- src/main/scala/org/aop4scala/Interceptor.scala
+++ src/main/scala/org/aop4scala/Interceptor.scala
@@ -12,7 +12,8 @@ trait Interceptor {
             false
   }
 
-  protected def matches(annotationClass: Class[T] forSome {type T <: Annotation}, invocation: Invocation): Boolean = {
+  protected def matches[T <: Annotation](annotationClass: Class[T], invocation: Invocation): Boolean = {
+
     invocation.method.isAnnotationPresent(annotationClass) ||
             invocation.target.getClass.isAnnotationPresent(annotationClass) ||
             false
--- src/main/scala/org/aop4scala/ManagedComponent.scala
+++ src/main/scala/org/aop4scala/ManagedComponent.scala
@@ -3,14 +3,17 @@ package org.aop4scala
 import java.lang.reflect.{Method, InvocationHandler, Proxy}
 
 object ManagedComponentFactory {
-  def createComponent[T](intf: Class[T] forSome {type T}, proxy: ManagedComponentProxy): T =
+  
+  def createComponent[T: ClassManifest](proxy: ManagedComponentProxy): T = {
+    val intf = implicitly[ClassManifest[T]]
     Proxy.newProxyInstance(
       proxy.target.getClass.getClassLoader,
-      Array(intf),
+      Array(intf.erasure),
       proxy).asInstanceOf[T]
+  }
 }
 
-class ManagedComponentProxy(val target: AnyRef) extends InvocationHandler {
+class ManagedComponentProxy(val target: AnyRef) extends InvocationHandler with Interceptor {
   def invoke(proxy: AnyRef, m: Method, args: Array[AnyRef]): AnyRef = invoke(Invocation(m, args, target))
 
   def invoke(invocation: Invocation): AnyRef = invocation.invoke
