0ef2203265e17eb35ae269e1ba48705a869bbd6c
FaKod <info@fakod.eu>
2010-07-12 22:08:40 +0200

    interim checkin

--- src/test/scala/org/aop4scala/test/InterceptorTest.scala
+++ src/test/scala/org/aop4scala/test/InterceptorTest.scala
@@ -1,7 +1,8 @@
 package org.aop4scala.test
 
 import org.specs.SpecificationWithJUnit
-import org.aop4scala.{ManagedComponentFactory, ManagedComponentProxy}
+import org.aop4scala.{Invocation, Interceptor, ManagedComponentFactory, ManagedComponentProxy}
+import java.lang.reflect.{Proxy, Method, InvocationHandler}
 
 /**
  * User: FaKod
@@ -9,15 +10,52 @@ import org.aop4scala.{ManagedComponentFactory, ManagedComponentProxy}
  * Time: 08:43:08
  */
 
+class Aspect(val pointcut: String) extends Interceptor {
+
+  def create[I: ClassManifest](target: AnyRef): I = {
+
+    val interface = implicitly[ClassManifest[I]]
+    
+    val proxy = new TargetProxy(this, target)
+
+    Proxy.newProxyInstance(
+      target.getClass.getClassLoader,
+      Array(interface.erasure),
+       proxy).asInstanceOf[I]
+  }
+
+  def invoke(invocation: Invocation): AnyRef = invocation.invoke
+}
+
+
+
+class TargetProxy(val aspect: Aspect, val target: AnyRef) extends InvocationHandler with Interceptor {
+  def invoke(proxy: AnyRef, m: Method, args: Array[AnyRef]): AnyRef = invoke(Invocation(m, args, target))
+
+  def invoke(invocation: Invocation): AnyRef = aspect.invoke(invocation)
+}
+
+
+
 class InterceptorTest extends SpecificationWithJUnit {
   "Interceptor" should {
 
+//    "enable logging intercept" in {
+//      var foo = ManagedComponentFactory.createComponent[Foo](
+//        new ManagedComponentProxy(new FooImpl)
+//                with LoggingInterceptor
+//                with TransactionInterceptor
+//        )
+//
+//      foo.foo("foo")
+//      foo.bar("bar")
+//    }
+
     "enable logging intercept" in {
-      var foo = ManagedComponentFactory.createComponent[Foo](
-        new ManagedComponentProxy(new FooImpl)
-                with LoggingInterceptor
-                with TransactionInterceptor
-        )
+
+      val aspect = new Aspect("execution(* *.foo(..))") with LoggingInterceptor with TransactionInterceptor
+
+      val foo = aspect.create[Foo](new FooImpl)
 
       foo.foo("foo")
       foo.bar("bar")
