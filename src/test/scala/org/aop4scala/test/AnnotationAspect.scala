39ea9af3ba2ad0f3668513776d2b393743bf61a9
FaKod <info@fakod.eu>
2010-07-14 21:40:23 +0200

    added a more complex Aspect example

--- src/test/scala/org/aop4scala/test/AnnotationAspect.scala
+++ src/test/scala/org/aop4scala/test/AnnotationAspect.scala
@@ -11,18 +11,38 @@ import org.aspectj.lang.annotation.{Aspect, Around, Before, Pointcut}
 
 @Aspect
 class AnnotationAspect {
-  
+
+  /**
+   * simple pointcut for execution of method lolli
+   */
   @Pointcut("execution(* *.lolli(..))")
   def call2Lolli() = {}
 
+  /**
+   * intercepts before execution of lolli
+   */
   @Before("call2Lolli()")
   def callFromFoo() = println("before executing lolli")
 
+  /**
+   * around execution of lolli
+   */
   @Around("call2Lolli()")
-  def doNothing(thisJoinPoint:ProceedingJoinPoint):Object = {
+  def doNothing(thisJoinPoint: ProceedingJoinPoint): Object = {
     println("Around Enter executing lolli")
     val result = thisJoinPoint.proceed
     println("Around Exit executing lolli")
     result
   }
+
+
+  /**
+   * more complex pointcut to get instance of LolliPop
+   * and get String parameter of method lolli 
+   */
+  @Pointcut("call(* *.lolli(String)) && args(s) && target(callee)")
+  def someCall(s: String, callee: LolliPop): Unit = {}
+
+  @Before("someCall(str, callee)")
+  def callFromSomeCall(str:String, callee: LolliPop) = println("before call with: " + str)
 }
